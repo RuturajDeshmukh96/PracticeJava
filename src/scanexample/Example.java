99adb03507c338abad775bc0e4e6f5ca79f19d3d
Ruturaj Deshmukh <ruturajdeshmukh96@gmail.com>
2026-06-22 22:52:41 +0530

    Initial commit fixed

---
+++ src/scanexample/Example.java
@@ -0,0 +1,24 @@
+package scanexample;
+import java.util.*;
+
+
+public class Example {
+    public  static void main (String []  args){
+
+        Scanner sc = new Scanner (System.in );
+
+        System.out.println("Addition");
+
+       System.out.println("Enter the frist number");
+       int a = sc.nextInt();
+
+       System.out.println("Enter the seconf number ");
+       int b = sc.nextInt();
+
+
+        int sum = a+ b ;
+        System.out.print("the total addition is ");
+        System.out.println(sum);
+        sc.close();
+    }
+}
