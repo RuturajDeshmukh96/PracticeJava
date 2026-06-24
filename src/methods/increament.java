c4c5dcbef4ed70c181d04cfacf9d8be335cb9d7a
Ruturaj Deshmukh <ruturajdeshmukh96@gmail.com>
2026-06-24 19:07:29 +0530

    practice

---
+++ src/methods/increament.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class increament {
+
+
+    public static void main(String [] args){
+
+        int y = 2 ;
+        int z = 5 ;
+        System.out.println(  ++ y *8);
+
+        int a = 10 ;
+
+        int j = 10 ;
+
+        System.out.println(++a * j);
+        System.out.println( z++);
+        System.out.println(++ z);
+    }
+}
