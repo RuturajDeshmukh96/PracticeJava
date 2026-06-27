e1dae74fcc0a15b3329cb5224b466b5e42f4de07
Ruturaj Deshmukh <ruturajdeshmukh96@gmail.com>
2026-06-27 20:58:10 +0530

    practice...

---
+++ src/string/New.java
@@ -0,0 +1,9 @@
+package string;
+
+public class New {
+    public static void main (String []  args ){
+        String name = ("i love coding ");
+        name = name.replace("love ",   "Hate" );
+        System.out.println(name );
+    }
+}
