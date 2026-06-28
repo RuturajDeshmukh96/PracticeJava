3e06df29584458015383f6ff051da876075ece11
Ruturaj Deshmukh <ruturajdeshmukh0096@gmail.com>
2026-06-28 22:54:39 +0530

    core practice

---
+++ src/Conditions/Prac.java
@@ -0,0 +1,32 @@
+package Conditions;
+import  java .util.*;
+public class Prac {
+    public static void main(String[] aegs) {
+
+        System.out.println("enter your age") ;
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+
+        switch (age) {
+
+            case 18:
+                System.out.println("you are able to vote in india ");
+                break;
+
+            case 21:
+                System.out.println(" you are mature now");
+                break;
+
+            case 30:
+                System.out.println(" you are senior citizens now");
+                break;
+
+            case 50:
+                System.out.println(" you are  at the age of retirement  now ");
+                break;
+            default:
+                System.out.println(" just live healthy life ");
+            }
+
+        }
+    }
