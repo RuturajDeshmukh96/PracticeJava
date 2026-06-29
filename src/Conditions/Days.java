4bfa5049621b99617a44c462d396c017a5f24401
Ruturaj Deshmukh <ruturajdeshmukh0096@gmail.com>
2026-06-29 20:35:13 +0530

    Days  practice

---
+++ src/Conditions/Days.java
@@ -0,0 +1,25 @@
+package Conditions;
+import java.util.*;
+public class Days {
+    public static void main(String [] args) {
+
+        System.out.println("Enter the days : ");
+
+        Scanner scan = new Scanner(System.in);
+
+        int day = scan.nextInt();
+        switch (day) {
+
+            case 1 -> System.out.println("Sunday");
+            case 2 -> System.out.println("Monday");
+            case 3 -> System.out.println("Tuesday");
+            case 4 -> System.out.println("Wednsday");
+            case 5 -> System.out.println("Thursday");
+            case 6 -> System.out.println("Friday");
+            case 7 -> System.out.println("Saturday");
+
+
+        }
+
+    }
+}
