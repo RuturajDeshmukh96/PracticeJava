cacec2dcb5c2896829dcec0b43e61dd5a91d4745
Ruturaj Deshmukh <ruturajdeshmukh0096@gmail.com>
2026-07-01 15:50:24 +0530

    __practice__

---
+++ src/Conditions/Decrement.java
@@ -0,0 +1,20 @@
+package Conditions;
+
+public class Decrement {
+
+    public static void main(String[] args) {
+/*
+
+        for (int a = 100; a >= 95; a--) {
+            System.out.println(a);
+
+        }
+    }
+}
+*/
+
+        for (int a = 50; a >= 45; a--) {
+            System.out.println(a);
+        }
+    }
+}
---
+++ src/Practice/Test.java
@@ -0,0 +1,38 @@
+package Practice;
+
+public class Test {
+
+  /* public static void main(String[] args) {
+        int a = 1;
+        while (a <= 20) {
+            System.out.println(a);
+
+            a++;
+        }
+    }
+}
+      */
+    /*
+  public static  void main (String [] args ){
+
+      int  r = 1;
+
+      while (r <= 20 ){
+
+          System.out.println("Ruturaj Deshmukh");
+          r++;
+      }
+  }
+}
+*/
+  public static void main (String [] args ){
+      int a = 2 ;
+
+      do {
+        System.out.println(a);
+        a++;
+      }
+      while (a <=100);
+
+}
+}
