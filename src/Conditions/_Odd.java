c4b87e11c3289befb389820a506dd5f69f58181b
Ruturaj Deshmukh <ruturajdeshmukh0096@gmail.com>
2026-07-01 15:50:24 +0530

    __practice__

---
+++ src/Conditions/_Odd.java
@@ -0,0 +1,17 @@
+package Conditions;
+import java.util.*;
+public class _Odd {
+  public static void main (String [ ] args ) {
+
+      Scanner scan = new Scanner (System.in);
+      System.out.println("enter the start number:");
+      //int a = scan.nextInt();
+       int a = 3;
+      for ( int  i = 0 ; i<= a; i++ ){
+          System.out.println(2*i+1);
+      }
+
+  }
+
+}
+
