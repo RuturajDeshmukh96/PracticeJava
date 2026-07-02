ab95d6b570c3b61109cde48d82a02ab3576ba7ac
Ruturaj Deshmukh <ruturajdeshmukh0096@gmail.com>
2026-07-02 18:26:39 +0530

    add

---
+++ src/string/Test.java
@@ -0,0 +1,11 @@
+package string;
+
+
+
+public class Test {
+    public static void main (String []  args ){
+        String name = ("i love coding ");
+        name = name.replace("love ",   "Hate" );
+        System.out.println(name );
+    }
+}
