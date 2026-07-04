da030c5668fdfc91544a9326359b62fc89b5f964
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-04 22:45:56 +0530

    array

---
+++ src/NEXT_CHAPTER/Array.java
@@ -0,0 +1,17 @@
+package NEXT_CHAPTER;
+
+public class Array {
+    public static void main (String []  args){
+
+
+        int [] marks = new int  [5];
+
+       marks [0]= 10;
+       marks [1] = 11;
+       marks [2] = 9;
+       marks [3] =13;
+       marks [4] = 14;
+       System.out.println(marks [4]);
+
+    }
+}
