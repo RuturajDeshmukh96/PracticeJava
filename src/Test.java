178c3707090304d825b1b6594a61dab63c0a3b97
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-04 20:55:52 +0530

    test1

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String [] args ){
+
+        for (int i = 25 ; i > 10 ; i--){
+            for (int j = 35 ;  j > i ; j--){
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
