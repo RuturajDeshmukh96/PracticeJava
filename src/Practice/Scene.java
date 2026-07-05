4efacc64abc741b85f0c66e6628ebae487b3b08e
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-05 12:10:14 +0530

    done

---
+++ src/Practice/Scene.java
@@ -0,0 +1,17 @@
+package Practice;
+import java.util.*;
+public class Scene {
+    public static void main (String [] args  ){
+
+        System.out.println("Check your seat number Here ");
+        Scanner scan = new Scanner(System.in);
+          int Seatno = scan.nextInt();
+
+          if ( Seatno % 2 == 0){
+              System.out.println(" Your seat is at Right side ");
+
+
+        }else {System.out.println("your seat is at left side ");}
+
+    }
+}
