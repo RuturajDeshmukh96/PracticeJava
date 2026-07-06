bd5a315085aa8d75fe37e418e73a711bfab9114a
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-06 14:12:57 +0530

    done1

---
+++ src/NEXT_CHAPTER/Array3.java
@@ -0,0 +1,28 @@
+package NEXT_CHAPTER;
+
+
+
+public class Array3 {
+    public static void main (String [] args ){
+
+
+        /*
+        int [] score = {11,22,33,44,55,66,77,};
+       // String name = {a,b,c,d,e,f,g,h};
+
+        for (int i = 1 ; i >= score ; i++);
+            System.out.println(score[i]);
+
+    }
+}
+
+    */
+
+            int[] numbers = {10, 20, 30, 40, 50};
+
+            System.out.println("Array elements:");
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println("Element at index " + i + ": " + numbers[i]);
+            }
+        }
+    }
---
+++ src/NEXT_CHAPTER/To.java
@@ -0,0 +1,16 @@
+package NEXT_CHAPTER;
+
+public class To {
+
+    public static void main(String[] args) {
+        int[] prices = {5, 12, 3, 8, 2};
+        int sum = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            sum += prices[i];
+        }
+
+        System.out.println("The total sum is: " + sum);
+    }
+}
+
