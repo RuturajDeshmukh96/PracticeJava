5896496662b35022c6d4e88437b3f94497640cd4
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-08 20:38:01 +0530

    ArrayRevrseprintMethod2

--- src/NEXT_CHAPTER/Array_reverse_print.java
+++ src/NEXT_CHAPTER/Array_reverse_print.java
@@ -3,6 +3,7 @@ package NEXT_CHAPTER;
 public class Array_reverse_print {
 
 //        public static void main(String[] args) {
+ //   System.out.println("Printing array  ");
 //            String[] Names = {"Ruturaj ", "Shahu", "Deshmukh"};
 //            for (int i =2; i <Names.length; i--) {
 //                System.out.println(Names[i]);
@@ -13,7 +14,9 @@ public class Array_reverse_print {
 
     public static void main(String[] args) {
         String[] Names = {"Ruturaj ", "Shahu", "Deshmukh"};
+        System.out.println("Printing array in reverse direction ");
         for (int i =Names.length-1; i>=0; i--) {
+
             System.out.println(Names[i]);
         }
     }
