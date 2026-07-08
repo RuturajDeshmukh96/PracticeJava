27f8d76c8cfcaa54d1a52af97fb6e55806628d40
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-08 19:45:47 +0530

    Array 1

---
+++ src/NEXT_CHAPTER/Chapter1.java
@@ -0,0 +1,14 @@
+package NEXT_CHAPTER;
+
+public class  Chapter1 {
+
+    public static void main (String [] args ){
+
+        int []  marks = {66,55,44,11,22,33,};
+        float [] score = {61.1f,88.9f,86.9f};
+        String [] names = {"raj","ratn ","rohan","jayesh "};
+        System.out.println(marks.length);
+        System.out.println(score [2]);
+        System.out.println(names.length );
+    }
+}
