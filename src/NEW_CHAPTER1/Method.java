55b76bc5b460d528bad9b8031f206d62ec9fe46e
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-09 20:31:46 +0530

    method

---
+++ src/NEW_CHAPTER1/Method.java
@@ -0,0 +1,13 @@
+package NEW_CHAPTER1;
+
+public class Method {
+    public static void main (String [] args ) {
+
+        thor("ironam ");
+        thor("Avengars");
+
+    }
+        public static void thor (String name ){
+      System.out.println("Thor says hello to " +  name );
+    }
+}
