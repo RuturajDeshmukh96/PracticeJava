c3e2af554bb39f27306c0aa70ab86399153a432e
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-09 14:17:40 +0530

    Array

--- src/NEXT_CHAPTER/Array4.java
+++ src/NEXT_CHAPTER/Array4.java
@@ -3,6 +3,7 @@ package NEXT_CHAPTER;
 public class Array4 {
 
     public static void main(String[] args) {
+        System.out.println("printing the array");
         String[] Names = {"Ruturaj ", "Shahu", "Deshmukh"};
         for (int i = 0; i < Names.length; i++) {
             System.out.println(Names[i]);
