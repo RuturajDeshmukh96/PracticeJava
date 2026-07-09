1ebd6b5031a0a476107ecae71b4cf258dd2ef3bb
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-09 21:06:55 +0530

    Addition of the array

---
+++ src/NEW_CHAPTER1/AdditionofArraay.java
@@ -0,0 +1,15 @@
+package NEW_CHAPTER1;
+
+public class AdditionofArraay {
+    public static void main (String [] args ){
+
+        float[] marks = {44.0f,45.9f,89.4f,94.5f,69.9f};
+        float sum = 0 ;
+
+        for (float rum : marks){
+            sum = sum + rum ;
+        }
+            System.out.println(sum);
+        }
+    }
+
