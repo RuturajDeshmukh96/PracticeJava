a93d82fb08c908bc897e047cbad48bb28ce90f43
RuturajDeshmukh96 <ruturajdeshmukh0096@gmail.com>
2026-07-10 21:03:47 +0530

    method

---
+++ src/Practice/N1.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class N1 {
+    public static void main (String [] args ) {
+
+        thor ("Fight");
+        thor ("Do or Die ");
+    }
+    public static void thor (String name ){
+        System.out.println("The Thor is warning for avengars :"+ name);
+    }
+}
