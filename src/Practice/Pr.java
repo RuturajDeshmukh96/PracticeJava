8179543d4c3725d58bb592a90fff7c07d90f682e
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-12 00:16:50 +0530

    foramt

---
+++ src/Practice/Pr.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Pr {
+    public static void main(String [] args ){
+
+
+        System.out.format("%-10s %-5s\n", "नाव", "गुण");
+        System.out.format("%-10s %-5d\n", "ऋतुराज", 95);
+        System.out.format("%-10s %-5d\n", "भावा", 88);
+
+System.out.println(" Now print using the printf");
+        System.out.println("   ");
+
+        System.out.printf("%-10s %-5s\n", "नाव", "गुण");
+        System.out.printf("%-10s %-5d\n", "Ruturaj", 95);
+        System.out.printf("%-10s %-5d\n", "Rutu", 88);
+
+    }
+}
