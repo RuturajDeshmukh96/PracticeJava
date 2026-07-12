d81acbd6417a8b44c4839bdcae2c49927a38fa35
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-12 23:19:54 +0530

    practice

--- src/Practice/M1.java
+++ src/Practice/M1.java
@@ -4,14 +4,14 @@ public class M1 {
 
     public static void main(String[] args) {
 
-        RohitSharma("Rohit sharma  & Pull Shot is my favorite shot ");
-        ViratKohali("Virat kohali & Cover Drive is my favorite shot");
+        RohitSharma("Rohit Sharma  & Pull Shot is my favorite shot ");
+        ViratKohali("Virat Kohali & Cover Drive is my favorite shot");
     }
 
     public static void RohitSharma(String name) {
-        System.out.println("The commentator ask to to audience1 tell me your faavorite crickter & shot :- " + name);
+        System.out.println("The commentator ask to to audience1 tell me your favorite crickter & shot :- " + name);
     }
         public static void ViratKohali(String name){
-            System.out.println("The commentator ask to to audience2 tell me your faavorite crickter & shot :- " + name);
+            System.out.println("The commentator ask to to audience2 tell me your favorite crickter & shot :- " + name);
         }
     }
