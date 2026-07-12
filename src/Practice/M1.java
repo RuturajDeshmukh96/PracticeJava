96c3b802550bb32509dd9c0e7f5bbc5de92f6b62
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-12 23:17:53 +0530

    practice

---
+++ src/Practice/M1.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class M1 {
+
+    public static void main(String[] args) {
+
+        RohitSharma("Rohit sharma  & Pull Shot is my favorite shot ");
+        ViratKohali("Virat kohali & Cover Drive is my favorite shot");
+    }
+
+    public static void RohitSharma(String name) {
+        System.out.println("The commentator ask to to audience1 tell me your faavorite crickter & shot :- " + name);
+    }
+        public static void ViratKohali(String name){
+            System.out.println("The commentator ask to to audience2 tell me your faavorite crickter & shot :- " + name);
+        }
+    }
