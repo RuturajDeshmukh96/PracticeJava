ab16dda3ff284df66f6fa349543ce7b8f9030b08
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-13 22:39:35 +0530

    method

---
+++ src/methods/Meth.java
@@ -0,0 +1,25 @@
+package methods;
+
+public class Meth {
+
+
+
+    public static void Showmessage (){
+        System.out.println("Welcome in the calculator ");
+
+    }
+    public static int Multiply( int num1 ,int num2){
+        int answer = num1 * num2 ;
+ return answer;
+    }
+
+
+    public static void  main (String [] args ){
+
+        Showmessage();
+
+       int myresult= Multiply (8,2);
+       System.out.println("the multiplication of given values are " + myresult);
+
+    }
+}
