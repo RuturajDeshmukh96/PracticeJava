07e7cc1f58ace36f179396897ebd2616b9c42595
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-13 23:07:27 +0530

    method

---
+++ src/methods/Method1.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class Method1 {
+
+    public static void Show(){
+        System.out .println("welcome to the calculation");
+
+    }
+    public static int  Cal(int a , int b){
+        int result =  a + b ;
+        return result ;
+            }
+
+            public static void main (String [] args ){
+
+               Show();
+
+               int answer = Cal(45 ,56);
+
+               System.out.println(answer);
+
+
+            }
+}
