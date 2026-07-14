085946599f7b8bbb3f2f329e853b2bb3f5cf77a6
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-14 21:26:18 +0530

    practice

---
+++ src/methods/MethodEx1.java
@@ -0,0 +1,15 @@
+package methods;
+//mport java.awt.*;
+import java.util.Scanner;
+public class MethodEx1 {
+    public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("let's Start...!!!");
+    System.out.println("Enter Your Name..!");
+     System.out.println("");
+       String Name =  scan.nextLine();
+
+       System.out.println("Let's Start the Game "+ Name);
+}
+
+}
