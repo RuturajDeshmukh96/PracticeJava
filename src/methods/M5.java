960d4fbf962d31c8d5d4c620a3e36262fc5f35e7
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-14 22:17:16 +0530

    practice

---
+++ src/methods/M5.java
@@ -0,0 +1,14 @@
+package methods;
+import java.util.*;
+public class M5 {
+    public static void main (String [] args ){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name Here:");
+        String name = sc.nextLine();
+        System.out.println("  ");
+        System.out.println("Your Good Name Is  :--> ");
+        System.out.println("  ");
+        System.out.println(name);
+    }
+}
--- src/methods/MethodEx1.java
+++ src/methods/MethodEx1.java
@@ -5,8 +5,8 @@ public class MethodEx1 {
     public static void main(String [] args){
     Scanner scan = new Scanner(System.in);
     System.out.println("let's Start...!!!");
-    System.out.println("Enter Your Name..!");
-     System.out.println("");
+    System.out.println("Enter Your Name..!!!");
+     System.out.println(" ");
        String Name =  scan.nextLine();
 
        System.out.println("Let's Start the Game "+ Name);
