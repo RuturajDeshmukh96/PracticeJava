113efa72b29672efafb80ea4e64762e0df2f112c
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-14 19:45:18 +0530

    practice

---
+++ src/Practice/N3.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class N3 {
+
+    public static void faktaNawSanga(String naw) {
+        System.out.println("my name is" + naw );
+
+    }
+
+    public static void main(String[] args) {
+        faktaNawSanga("Ramesh");
+    }
+}
