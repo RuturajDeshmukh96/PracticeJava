f86d01f5f51361bf33c9a1f85e54c70b5c6b93e0
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-15 00:15:08 +0530

    non static practice

---
+++ src/Practice/Practice_1.java
@@ -0,0 +1,25 @@
+package Practice;
+
+public class Practice_1 {
+
+    public static void main (String [] args ){
+
+        Practice_1 p1 = new Practice_1();
+        p1.Rohit();
+        p1.Sachin();
+        p1.Virat();
+    }
+
+    public void Rohit(){
+
+        System.out.println("Rohit Sharma is playing pull shot Smoothly");
+    }
+    public void Virat(){
+
+        System.out.println("Virat kohali is  playing drive Smoothly");
+    }
+    public void Sachin(){
+
+        System.out.println("Sachin ia playing Straight Drive ");
+    }
+}
