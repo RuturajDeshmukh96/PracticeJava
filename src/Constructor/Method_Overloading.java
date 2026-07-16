8826ebe530ec0ff07a4256ec971d19096ea73d74
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-16 15:02:46 +0530

    practice overloading

---
+++ src/Constructor/Method_Overloading.java
@@ -0,0 +1,23 @@
+package Constructor;
+
+public class Method_Overloading {
+
+
+    public static int add(int a , int b ){
+        System.out.println("This is printing the method one with 2 paramter ");
+        return a+ b;
+    }
+
+    public static int add(int a , int b , int c){
+        System.out.println("This is printing the method 2 with 3 paramter ");
+        return a+b+c ;
+
+    }
+    public static void main (String [] dj){
+        System.out.println(add(11,22));
+        System.out.println(add(22,33,43));
+
+
+
+    }
+}
