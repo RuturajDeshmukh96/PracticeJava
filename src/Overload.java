aa039151ac7f5a1b112992ed7231efe8a6033cc4
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-16 14:06:34 +0530

    practice overloading

--- src/Overload.java
+++ src/Overload.java
@@ -1,16 +1,20 @@
-//public class Overload {
-//
-//public static void add (int a, int b ){
-//    System.out.println("calling the addding method with 2 parameter  ");
-//    return a + b;
-//}
-//
-//public static void add (int a , int  b ){
-//    System.out.println("calling the methid with 3 parameter ");
-//    return a + b + c ;
-//
-//}
-//public static void main (String [] gnya )
-//}
-//
-//
+public class Overload {
+
+public static int add (int a, int b ){
+    System.out.println("calling the addding method with 2 parameter  ");
+    return a + b;
+}
+
+public static int add (int a , int  b ,int c ){
+    System.out.println("calling the methid with 3 parameter ");
+    return a + b + c ;
+
+}
+public static void main (String [] gnya ){
+  System.out.println(add(12,21));
+    System.out.println(  add(12,23,34));
+
+}
+}
+
+
