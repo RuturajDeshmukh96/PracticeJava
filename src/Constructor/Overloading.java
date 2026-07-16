dd9870fa4860fc91ca49c07d72f9cacbc83e6e24
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-16 22:03:13 +0530

    practice overloading

---
+++ src/Constructor/Overloading.java
@@ -0,0 +1,22 @@
+package Constructor;
+
+public class Overloading {
+
+    public  String run(int a, String thor ){
+        System.out.println(a+ " " + thor );
+        return thor ;
+    }
+    public void  run(float a , Boolean done){
+        System.out.println(a+ " " + done);
+       // return done. toString();
+    }
+
+
+    public static void main (String [] law){
+
+        Overloading fu = new Overloading();
+
+       fu. run(15,"iron");
+       fu.run (400.00f,true);
+    }
+}
