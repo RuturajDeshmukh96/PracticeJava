13818cb8fb2fe4a2fcd0a5d0dd6f795bb65137b7
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-19 23:33:03 +0530

    overloading zomato

---
+++ src/Scenario_Based/Zomato.java
@@ -0,0 +1,35 @@
+package Scenario_Based;
+import java.util.Scanner ;
+ class Zomato {
+
+    public void order(String thali,int plates ) {
+        System.out.println("your"+ " " + thali + " "+"is ready to go please wait for 5 min  "+ " you ordered  " + plates +" "+ "plate");
+    }
+    public void order(String colddrink){
+        System.out.println(" And also you orderrd "+ " " + colddrink +" " +  "colddrink");
+    }
+
+}
+ class Delivary  {
+
+    public static void main (String [] raje){
+
+        Zomato order= new Zomato ();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to zomato order please..!@ ");
+        String food =scan.next();
+
+        System.out.println("how many plates do you want ");
+        int count = scan.nextInt();
+
+
+
+        System.out.println("any coldrink you need");
+        String cold = scan.next();
+
+        order.order(food,count);
+
+        order.order(cold);
+    }
+}
