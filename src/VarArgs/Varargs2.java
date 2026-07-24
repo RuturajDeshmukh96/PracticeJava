ec634d168f4849064e19bd3cb5b6981dceb5fb32
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-24 23:09:01 +0530

    Varargs4

--- src/VarArgs/Varargs2.java
+++ src/VarArgs/Varargs2.java
@@ -1,4 +1,21 @@
 package VarArgs;
 
 public class Varargs2 {
+
+    static void Run(int... score ){
+        System.out.println("this is the exapmle of the varargs ");
+
+        int total = 0 ;
+        for (int R : score ){
+            total = total + R;
+            System.out.println(score.length + total );
+            System.out.println(total );
+        }
+    }
+    public static void main (String [] ars){
+
+        Run(33,11);
+        Run (56,66,66,6655,42,2);
+        Varargs2.Run(77,56,99,87);
+    }
 }
