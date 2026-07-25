423145aa49af800c96f1e706d73bbc612ec0acb0
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-25 19:35:34 +0530

    overloading practice

---
+++ src/Poly/Methodoverloading.java
@@ -0,0 +1,19 @@
+package Poly;
+
+public class Methodoverloading {
+
+    public static void Rutu (int a , int b){
+        System.out.println("This is prrinting the frist parameter :" + a+" " + b) ;
+
+    }
+    public static void Rutu(String c, String d ){
+        System.out.println("this is the second parameter: " +c +d);
+    }
+public static void main(String [] raje ){
+        Rutu(22,33);
+        Rutu("this ","run ");
+
+
+}
+
+}
