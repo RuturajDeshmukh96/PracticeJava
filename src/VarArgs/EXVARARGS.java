28b7cb127b73d57c6fe96f00f3142df5b3c0b47a
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-25 23:50:43 +0530

    Varargs

---
+++ src/VarArgs/EXVARARGS.java
@@ -0,0 +1,15 @@
+package VarArgs;
+
+public class EXVARARGS {
+    public static void Runs (int... score ){
+        System.out.println("lets check the runs ");
+        int total =0 ;
+        for (int r : score ){
+            total  = total = r ;
+            System.out.println(total );
+        }
+    }
+    public  static void main (String []  args){
+        Runs(55,55);
+    }
+}
