0ccb9e1a964ec56f8a381563067a68169de7e6de
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-26 21:43:33 +0530

    factorial from user input

---
+++ src/FACTORIAL/FACT1.java
@@ -0,0 +1,18 @@
+package FACTORIAL;
+import java.util.Scanner;
+public class FACT1 {
+
+    public static void main(String [] raje ){
+
+        Scanner Scan = new Scanner (System.in);
+        System.out.println("Enter the number here which factorial you want :  ");
+        int number  = Scan.nextInt();
+        int fact = 1 ;
+
+        for (int i = 1 ; i <= number ; i++){
+            fact = fact * i ;
+        }
+        System.out .println(number +""+"'s factorial is the "+ fact);
+
+    }
+}
