947672cadd7974a4eb2b5b1493903c2701926abc
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-29 16:14:28 +0530

    factorial 1:

---
+++ src/FACTORIAL/FACT6.java
@@ -0,0 +1,13 @@
+package FACTORIAL;
+
+public class FACT6 {
+    public static void main (String [] raje ){
+       int number = 5;
+               int fact = 1;
+
+        for (int i = 1 ; i <= number ; i++){
+            fact = fact * i ;
+        }
+        System.out.println(fact);
+    }
+}
--- src/Practice/Palindrome.java
+++ src/Practice/Palindrome.java
@@ -19,7 +19,7 @@ public class Palindrome {
        if (ori  == reverse ) {
            System.out.println(ori + " : is a palindrome number");
        }else{
-           System.out.println(ori +" :is not a palindrome number");
+           System.out.println(ori +" : is not a palindrome number");
        }
     }
 }
