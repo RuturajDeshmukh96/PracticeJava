d1c0af1c5c6ff527bf695784ac13ecad9d6ec4da
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-29 15:03:47 +0530

    palindrome user input

---
+++ src/Practice/Palindrome.java
@@ -0,0 +1,25 @@
+package Practice;
+import java.util.Scanner;
+public class Palindrome {
+    public static void main (String [] Raj ){
+       Scanner SC = new Scanner (System.in) ;
+       System.out.println("enter the number to check the number is palindrome or not ");
+       int number = SC.nextInt();
+       int ori = number ;
+       int reverse = 0 ;
+       while (number > 0 ){
+           int rem = number % 10 ;
+           reverse = reverse * 10 + rem ;
+           number = number / 10;
+
+       }
+       System.out.println("original number :" + number);
+       System.out.println("Reversed number :" + reverse);
+
+       if (ori  == reverse ) {
+           System.out.println(ori + " is a palindrome number");
+       }else{
+           System.out.println(ori +" is not a  palindrome number");
+       }
+    }
+}
