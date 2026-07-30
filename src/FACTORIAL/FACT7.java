f77a6afd953d0209a935dd6720c858890fc3dff5
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-30 18:45:26 +0530

    Scanner practice fact

---
+++ src/FACTORIAL/FACT7.java
@@ -0,0 +1,19 @@
+package FACTORIAL;
+import java.util.Scanner ;
+public class FACT7 {
+    public static void main (String []args ){
+        System.out.println("Enter your number  here");
+        Scanner Rut = new Scanner (System.in);
+        int num = Rut.nextInt();
+        int fact = 1;
+
+        for (int i = 1 ; i <= num ;i++){
+
+            fact = fact * i ;
+
+            System.out .println(fact);
+        }
+
+
+    }
+}
