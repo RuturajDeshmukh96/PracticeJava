cfb0f84e721468c5bfc36fdb78c77cfda15211db
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-07-31 19:43:17 +0530

    practice custom class

---
+++ src/Practice/Info.java
@@ -0,0 +1,21 @@
+package Practice;
+
+ class Info {
+
+        int id ;
+        String name ;
+    }
+    class CustomClass {
+    public static void main (String [] args ){
+System.out.println("this is our custom class");
+
+      Info rutu =new Info();
+      rutu.id= 33;
+      rutu.name  ="ruturaj ";
+      System.out.println(rutu.id);
+  System.out.println(rutu.name);
+
+    }
+
+
+}
