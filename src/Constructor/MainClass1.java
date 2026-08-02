877447c54870faff6b92f560677a4159b88cd331
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-02 23:14:01 +0530

    oops

--- src/Constructor/MainClass1.java
+++ src/Constructor/MainClass1.java
@@ -4,48 +4,73 @@ public class MainClass1 {
     int age ;
     int marks ;
     String name ;
-    public MainClass1(int age, int marks , String name ){
+    int grade;
+    public MainClass1(int age, int marks , String name ,float a ){
         this.age=age;
         this.marks=marks ;
         this.name=name;
-        System.out.println("this line is from contructor ");
-        System.out.println(age + " " + marks + " " + name );
+        this.grade=grade;
+        System.out.println("This line is from Constructor 1");
+        System.out.println(age + " " + marks + " " + name + " " + a);
     }
     public void Over(){
-        System.out.println("i am creating this for const overriding ");
+        System.out.println("I am creating this for const Overriding 2");
+    }
+    public void Showgrade(){
+        System.out.println("printing the grade of studeents " + grade);
     }
 }
 interface Magic2{
     default void run(){
-        System.out.println("this line is from interface");
+        System.out.println("This line is from Interface 3");
     }
     default void run1() {
-        System.out.println("second method of interfaace");
+        System.out.println("Second method of Interface 4");
+    }
+    default void run2(){
+        System.out.println("third para in interface with third method " );
     }
 }
 
+
 class Subclass1  extends MainClass1 implements Magic2 {
 
-    public Subclass1(int age,int marks ,String name ){
-        super(age,marks ,name );
+    public Subclass1(int age,int marks ,String name , int grade ){
+        super(age,marks ,name ,grade);
 
     }
     public static void main(String []args){
-        Subclass1 done  = new Subclass1 (21,89,"rutu") ;
+
+        System.out.println("--------this is line is from main method----------");
+
+
+        Subclass1 done  = new Subclass1 (21,89,"Ruturaj 👻☠️",86) ;
     done.run();
     done.run1 ();
     done.Over();
+    done.Showgrade();
+    done.run2();
     }
     @Override
     public void run(){
-        System.out.println("printing method  oveerridding  ");
+        System.out.println("printing method  Oveerridding  5");
         Magic2.super.run();
 
     }
     @Override
     public void Over(){
-        System.out.println("this is printing the const overridding ");
+        System.out.println("this is printing the const Overridding 6");
         super.Over();
 
+    }
+    @Override
+    public void run2(){
+        System.out.println("override in the interface 2nd time");
+        Magic2.super.run2();
+    }
+    @Override
+    public void Showgrade(){
+        System.out.println("override in const  2nd time");
+        super.Showgrade();
     }
 }
