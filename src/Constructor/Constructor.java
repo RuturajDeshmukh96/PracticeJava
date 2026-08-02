82c7f82847c60ffa23f4e90739ce6196c195c887
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-02 16:02:40 +0530

    praactice

--- src/Constructor/Constructor.java
+++ src/Constructor/Constructor.java
@@ -1,13 +1,72 @@
 package Constructor;
 
-public class Constructor {
+ class Constructor1 {
+int a ;
+int b;
 
-    public Constructor(){
+    public Constructor1(int a , int b){
+
+        this.a = a;
+        this.b=b;
+        System.out.println(a+b);
         System.out.println("this is the constructor example");
         }
+        public void hello(){
+        System.out.println("original hello");
+        }
     }
-class Two {
-    public static void main(String[] ars) {
-        Constructor One = new Constructor();
+   interface Magic {
+     void menu();
+     default void maya(){
+         System.out.println("this is diffault method from interface ");
+         maya1();
+
+     }
+     private void maya1(){
+         System.out.println("2nd private method from interface ");
+     }
+   }
+
+
+
+
+
+class Two extends Constructor1 implements  Magic {
+
+   public Two( int a , int  b ){
+
+       super(a,b);
+
+
+     }
+
+    public static void main(String[] ars)
+    {
+
+        Two obj = new Two (11,22);
+      obj.hello();
+      obj.menu();
+      obj.maya();
+
+
+      //  Constructor1 One = new Constructor1();
+    //One.hello();
     }
+    @Override
+    public void hello(){
+        System.out.println("hello from override ");
+
+        super.hello();
+        //super.
+    }
+
+    @Override
+
+    public void menu(){
+       System.out.println("oveerridden from  abstact method  ");
+
+    }
+
+
+
 }
