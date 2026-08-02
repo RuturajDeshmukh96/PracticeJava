1ca8ce24494dcc897df9642c7f93683d1c788ce1
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-02 20:03:20 +0530

    praactice constructor interface

--- src/Constructor/MainClass.java
+++ src/Constructor/MainClass.java
@@ -1,35 +1,46 @@
 package Constructor;
+
 public class MainClass {
-    int a ;
-    int b ;
-    String yz ;
-    public MainClass(int a, int b , String yz){
-        this.a =a ;
-        this.b =b ;
-        this.yz = yz ;
-        System.out.println(a+" " + b+ " " +yz);
+    int a;
+    int b;
+    String yz;
+
+    public MainClass(int a, int b, String yz) {
+        this.a = a;
+        this.b = b;
+        this.yz = yz;
+        System.out.println(a + " " + b + " " + yz);
         System.out.println("This is from the Constructor ");
     }
 }
-interface Magic {
-    void manu();
-    default void  manu1 (){
 
-        System.out.println("This is from the ");
+interface Magic1 {
+    public void manu();
+
+    default void manu_1() {
+        System.out.println("This is from the default method");
     }
+}
 
-        }
+class SubClass implements Magic1 {
 
-class SubClass implements Magic {
-    public static void main(String [] args ) {
+    public static void main(String[] args) {
         MainClass obj = new MainClass(12, 13, "done");
+
+        System.out.println("-------------------");
+
+        SubClass sub = new SubClass();
+        sub.manu();
+        sub.manu_1();
     }
-        @Override
-       public  void  manu1 (){
 
-            System.out.println("This is from the ");
-        }
-
+    @Override
+    public void manu() {
+        System.out.println("This is overridden manu method!");
     }
 
-
+    @Override
+    public void manu_1() {
+        System.out.println("This is overridden manu_1 method!");
+    }
+}
