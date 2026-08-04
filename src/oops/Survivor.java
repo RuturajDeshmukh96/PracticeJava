57e53151fce560305df0b2c36870add047425607
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-05 00:32:05 +0530

    oops 2

--- src/oops/Survivor.java
+++ src/oops/Survivor.java
@@ -1,5 +1,7 @@
 package oops;
 
+import javax.swing.*;
+
 public class Survivor {
     String playerName ;
      int health ;
@@ -8,7 +10,7 @@ public class Survivor {
         this.health = health;
     }
         public void heal () {
-             System.out.println(" player is using bandage and healing  ");
+             System.out.println("player is using bandage and healing  ");
 
         }
     }
@@ -18,52 +20,78 @@ interface CombatAction{
         System.out.println("Grenade thrown! BOOM! 💣");
     }
 }
-class zombie extends Survivor implements CombatAction{
-int ammo ;
-    public zombie (String playername , int health,int ammo ) {
-        super(playername,health);
-        this.ammo=ammo;
+class zombie extends Survivor implements CombatAction {
+    int ammo;
+
+    public zombie(String playername, int health, int ammo) {
+        super(playername, health);
+        this.ammo = ammo;
     }
 
     @Override
 
-    public void heal () {
-        System.out.println(" plyer is using the medikit for health boost..! ");
+    public void heal() {
+        System.out.println("plyer is using the medikit for health boost..! ");
 
     }
+
     @Override
-    public void meleeattack(){
+    public void meleeattack() {
         System.out.println("Swinging the Katana sword! Slash! ⚔️");
     }
 
-    public void shoot (){
-        if (health <=20) {
+    public void shoot() {
+        if (health >= 20) {
             System.out.println("Attack fire om enemy");
-        }else {
+        } else {
             System.out.println("use medilit need to cover..");
         }
     }
 
     public void shoot(int bulletsToFire) {
-      if (bulletsToFire <= ammo ){
-          this.ammo = this.ammo - bulletsToFire;
+        if (bulletsToFire <= ammo) {
+            this.ammo = this.ammo - bulletsToFire;
+            System.out.println("Fired " + bulletsToFire + " bullets! Remaining Ammo: " + this.ammo);
+        } else {
 
-      }else {
-          System.out.println("Fired " +  bulletsToFire + " bullets! Remaining Ammo: " + this.ammo);
-      }
+            System.out.println("Click click! Out of ammo! Reload! 🔄");
+
+        }
 
     }
 
-    public static void main(String [] args ) {
+    public static void main(String[] args) {
 
-        zombie war = new zombie("Ruturaj " ,100,500);
+        zombie war = new zombie("Ruturaj ", 100, 500);
         war.heal();
         war.meleeattack();
-        war.throwbomb ();
-        war.shoot ();
-        war.shoot (15);
-    }
+        war.throwbomb();
+        war.shoot();
+        war.shoot(115);
+        // war.headshot();
 
 
+    Sniper gun = new Sniper("ruturaj ", 100, 200);
+    gun.headshot();
+}
     }
+
+class Sniper extends Survivor{
+
+    int sniperAmmo;
+
+    public Sniper(String playername, int health , int sniperAmmo){
+        super("Ruturaj ", 100);
+        this.sniperAmmo=sniperAmmo;
+    }
+    public void headshot (){
+        if (sniperAmmo > 0 ){
+            this.sniperAmmo= sniperAmmo -1 ;
+            System.out.println("BOOM! 🎯 Perfect Headshot!");
+        }else {
+            System.out.println("Out of sniper ammo!");
+        }
+    }
+
+}
 
