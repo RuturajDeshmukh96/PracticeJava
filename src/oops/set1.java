54cb33a90a8fe80f3194fbea4d32ff8ede327be3
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-07 08:28:15 +0530

    practice

--- src/oops/set1.java
+++ src/oops/set1.java
@@ -4,5 +4,28 @@ public class set1 {
     private String name ;
     private int score ;
 
+    public set1(String name , int score) {
+        this.name =name ;
+        this.score = score  ;
+        System.out.println("this is the example o the encapsulaton");
+    }
+    public void setdetails() {
+        name = "Ruturaj ";
+        score = 99;
+    }
+    public  int getscore (){
+        return score ;
+    }
+    public String getname (){
+        return name ;
+    }
 
+public static void main (String [] args ){
+        set1 info = new set1("rutu",0) ;
+
+        info.setdetails();
+
+    System.out.println("the name is  "+info.getname());
+    System.out.println("the score is  "+info.getscore());
+}
 }
