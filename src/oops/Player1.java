852c7006867c92b2d09b624ab4008556a0ffd7b8
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-09 08:42:34 +0530

    practice

---
+++ src/oops/Player1.java
@@ -0,0 +1,25 @@
+package oops;
+
+public abstract class  Player1 {
+
+    private String name ;
+    private int hp ;
+    public Player1 (String  name, int  age ){
+        this.name =name ;
+        this.hp = hp ;
+    }
+    public void takedamage (int damage ){
+        hp = hp - damage ;
+        if (hp <= 0 ){
+            hp = 0 ;
+            System.out.println("knocked out ☠️");
+        }
+        
+    }
+    public void heal (int medikit ){
+        hp = hp +  medikit ;
+        System.out.println("Used Medkit! HP is now   " + hp);
+    }
+    public abstract void attack ();
+}
+class Snoiper extends 
