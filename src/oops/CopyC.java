0213131891a4e498b66f33e9e9a22aa882d94dc5
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-10 23:30:26 +0530

    practice

---
+++ src/oops/CopyC.java
@@ -0,0 +1,40 @@
+package oops;
+
+public class CopyC {
+    private String model ;
+    private String engine ;
+    private  String company ;
+    public CopyC (String model , String engine  , String company ){
+        this . model = model ;
+        this . engine =  engine   ;
+        this  . company =  company ;
+    }
+    public CopyC(CopyC New ){
+        this.model = New.model;
+        this.engine= New . engine ;
+        this.company = New . company ;
+
+    }
+    public void show (){
+        System.out.println("model of car is : " + model + "  engine is  : " +engine  + "  company  of the car is : " + company);
+    }
+    public void  change (){
+        model= model = "verna ";
+        engine = engine = " turboo " ;
+         company = company  = "volkswagon ";
+    }
+
+}
+class Root {
+    public static void main (String [] args ){
+        CopyC c =  new CopyC ("supraa","turbo v6 ","Toyota");
+        CopyC d = new CopyC(c);
+        System.out.println("Start ");
+        c.show();
+        System.out.println("lets make changes  ");
+        c.change();
+        System.out.println("final  ");
+        c.show();
+
+    }
+}
