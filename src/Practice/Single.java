68975364253a9e755736ce30d6d5babc6b25b9f3
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-11 21:58:07 +0530

    single  inheritacnce

---
+++ src/Practice/Single.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class Single {
+    public void frist () {
+        System.out.println(" this is the base clas ");
+    }
+}
+class Done extends Single {
+    public void Deo(){
+    System.out.println(" child 1 ");
+}
+public static void main(String [] arsg ){
+        Done p1 = new Done() ;
+        p1.frist();
+        p1.Deo();
+}
+}
