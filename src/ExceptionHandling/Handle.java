cb6be54fb19a9e2519cc68ecaf056fe0455622b7
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-12 23:18:38 +0530

    exception handling

---
+++ src/ExceptionHandling/Handle.java
@@ -0,0 +1,18 @@
+package ExceptionHandling;
+
+public class Handle {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10;
+            int b = 0;
+            System.out.println(a / b);
+        }catch (Exception e ){
+            System.out.println("something is wrong in this code");
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("this is the finally block and always run ");
+        }
+    }
+}
