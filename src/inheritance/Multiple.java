2fc31bbb8ca49d36bffa167ef45651250e9e54b1
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-12 16:49:31 +0530

    Innheritance

--- src/inheritance/Multiple.java
+++ src/inheritance/Multiple.java
@@ -4,6 +4,9 @@ public class Multiple {
 }
 interface Drive {
     void move  () ;
+    default void move1 () {
+        System.out.println("this is from the interface ");
+    }
 }
 interface Float{
     void move () ;
@@ -12,10 +15,11 @@ class Hower implements Drive , Float{
 
     @Override
     public void move() {
-        System.out.println("the multiple inheritance in the java  ");
+        System.out.println("the multiple Inheritance in the java  ");
     }
     public static void main (String [] arg){
         Hower p1  = new Hower();
         p1.move();
+        p1.move1();
     }
         }
