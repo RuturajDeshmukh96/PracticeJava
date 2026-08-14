4331e016675f2b77cbcc6e9337949c663474ce3b
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-14 23:55:04 +0530

    final

--- src/Practice/FInaal1.java
+++ src/Practice/FInaal1.java
@@ -4,8 +4,9 @@ public class FInaal1 {
     final static int a = 10;
     final static int b = 20;
 
+
     final static void Done() {
-        System.out.println("This is the end ");
+        System.out.println("This is the End ");
 
     }
 
@@ -20,6 +21,7 @@ public class FInaal1 {
     public FInaal1(String name, int age) {
         this.name = name;
         this.age = age;
+
     }
 
     public String getname() {
@@ -43,7 +45,16 @@ public class FInaal1 {
         }
     }
 }
-    class Dove extends FInaal1 {
+
+
+interface Chick {
+    void chicks ();
+    default void  chicks1 () {
+        System.out.println("This is the interface");
+    }
+}
+
+    class Dove extends FInaal1 implements Chick {
         public int score ;
         public Dove (String name , int age,int score  ) {
             super(name ,age);
@@ -55,9 +66,17 @@ public class FInaal1 {
             Add();
             test();
             Dove f1 = new Dove("Ruturaj", 22,96);
-
+             f1.chicks();
+             f1.chicks1();
             System.out.println("The Name is is : " +f1.getname());
-         System.out.println("The Age is" +f1.getage());
-            System.out.println("The Score is" + f1. score );
+         System.out.println("The Age is : " +f1.getage());
+            System.out.println("The Score is : " + f1. score );
+        }
+
+
+        @Override
+        public void chicks (){
+            System.out.println("absstract overroddding ");
+
         }
     }
