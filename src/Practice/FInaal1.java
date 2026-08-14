fb2b8558de5d92686626fa9d5fc2c7dd75c603d5
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-14 22:52:42 +0530

    final

---
+++ src/Practice/FInaal1.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class FInaal1 {
+    final static int a  =  10 ;
+    final static int b = 20 ;
+
+    final static  void Done  () {
+        System.out.println("This is the end ");
+
+    }
+    final  static void Add () {
+        System.out.println(a+b);
+
+    }
+    public static void main(String [] args ){
+        Done();
+        //System.out.println(Add());
+        Add();
+    }
+}
