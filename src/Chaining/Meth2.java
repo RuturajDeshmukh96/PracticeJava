d4a9bbf85e9eb47d7e4cf50393adc30c759fb46f
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-15 15:47:24 +0530

    practice

---
+++ src/Chaining/Meth2.java
@@ -0,0 +1,34 @@
+package Chaining;
+
+public class Meth2 {
+    String name ;
+    int age ;
+    float marks ;
+    boolean pass ;
+    public  Meth2 m1 (String name ) {
+        System.out.println("Frist method of class - name : "+name ) ;
+        return this ;
+    }
+    public Meth2 m2 (int age ){
+        System.out.println("Second method of class - age : "+ age );
+        return this ;
+    }
+    public Meth2 m3 (float marks ){
+        System.out.println("Third method of class - marks : " + marks  );
+        return this ;
+    }
+    public Meth2 m4 (boolean pass) {
+        System.out.println("Fourth method of the class - pass or fail : "+ pass);
+        if (pass = true ){
+            System.out.println("Pass");
+        }  else {
+                System.out.println("fail");
+        }
+
+        return this ;
+    }
+    public static void main (String [] args ){
+        Meth2 m = new Meth2 ();
+        m.m1("Ruturaj").m2(22).m3(89.00f).m4(true );
+    }
+}
