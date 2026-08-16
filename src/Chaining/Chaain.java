aa23d8f5cd44e1ffcb1d92f4ead8cd64a2dbcbee
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-16 14:29:50 +0530

    practice

--- src/Chaining/Chaain.java
+++ src/Chaining/Chaain.java
@@ -4,25 +4,21 @@ public class Chaain {
     String name ;
     int age ;
     float  score ;
-    public Chaain (String name){
-        System.out.println("thiss is printing the name : " + name  );
+    public Chaain (){
+        this ("Ruturaj ",89.00f , 22);
+        System.out.println("thiss is printing from the default constructor  : " );
     }
-    public Chaain (float  score ){
 
-        System.out.println("thiss is printing the score  : " + score  );
-    }
-    public Chaain (int age ){
-        System.out.println("printing the age : " + age );
-    }
-    public Chaain (String name, int score , int age  ){
+    public Chaain (String name, float  score , int age ){
         this . name = name ;
         this.score = score  ;
         this . age = age ;
+        System.out.println("The name of Stud is " + name + "The score of stud " + score );
     }
     public static void main(String [] args ) {
-        Chaain c= new  Chaain ("Rutu..!") ;
-        Chaain c1 = new Chaain (89.0f);
-        Chaain c2  =  new Chaain (22);
+        Chaain c= new  Chaain () ;
+
+
 
 
     }
