206493abd260b60d5e9427196f96980e5badde5d
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-16 16:04:42 +0530

    practice acess modifier protected, imported another and another class

---
+++ src/Practice/Protect.java
@@ -0,0 +1,19 @@
+package Practice;
+import ACCESS_MODIFIERS1.PROTECT ;
+
+
+public class Protect extends PROTECT {
+
+    public void  setName () {
+        System.out.println(name);
+    }
+
+    public static void main (String [] args ) {
+        Protect n = new Protect();
+        n.setName();
+
+    }
+
+
+    }
+
