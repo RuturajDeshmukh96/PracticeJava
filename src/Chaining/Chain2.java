3d2d968b2a614abff20e8ec55f78ad0bb25eec15
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-17 20:16:07 +0530

    practice

---
+++ src/Chaining/Chain2.java
@@ -0,0 +1,28 @@
+package Chaining;
+
+ public class Chain2 {
+
+     String name;
+     int age;
+     float score;
+
+     public Chain2() {
+         this("Ruturaj", 22, 89.00f);
+         System.out.println("The Frist Constructor ");
+     }
+
+     public Chain2(String name, int age, float score) {
+         this.name = name;
+         this.age = age;
+         this.score = score;
+         System.out.println("Lets start Constuctor Chaining ");
+         System.out.println("The Constructor Chaining is in working \n" + "It takes name age & score from the default constructor  : " + name + " " + age + " "  + score );
+
+     }
+
+     public static void main(String[] srgs) {
+         Chain2 w = new Chain2();
+     }
+ }
+
+
--- src/Practice/Protect.java
+++ src/Practice/Protect.java
@@ -1,19 +1,12 @@
 package Practice;
 import ACCESS_MODIFIERS1.PROTECT ;
-
-
 public class Protect extends PROTECT {
-
     public void  setName () {
         System.out.println(name);
     }
-
     public static void main (String [] args ) {
         Protect n = new Protect();
         n.setName();
-
     }
-
-
     }
 
