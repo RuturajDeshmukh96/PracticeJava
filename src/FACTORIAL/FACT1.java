1d53a78a4862918e2b454961d5754c824ddf7f42
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-19 21:45:18 +0530

    practice

--- src/FACTORIAL/FACT1.java
+++ src/FACTORIAL/FACT1.java
@@ -12,7 +12,7 @@ public class FACT1 {
         for (int i = 1 ; i <= number ; i++){
             fact = fact * i ;
         }
-        System.out .println(number +""+"'s factorial is the "+ fact);
+        System.out .println(number +""+"'  s factorial is the "+ fact);
 
     }
 }
