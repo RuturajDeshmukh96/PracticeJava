d4775c565a45e026fa8b38dbe74e09eb7b324ce8
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-21 08:20:41 +0530

    practice

---
+++ src/CollectionFrameworks/HashSett.java
@@ -0,0 +1,28 @@
+package CollectionFrameworks;
+import java.util.HashSet;
+public class HashSett {
+    String name ;
+    public HashSett (String  name ){
+        this.name =  name ;
+    }
+    public static void main (String [] args ){
+
+        HashSet<HashSett> done = new HashSet<>();
+        HashSett j = new HashSett("Ruturaj ");
+     HashSett j1 = new HashSett(" ganesh");
+     HashSett j2 = new HashSett(" digvijay ");
+     HashSett j3 = new HashSett("om");
+     HashSett j4 = new HashSett("om");
+
+     done.add(j);
+     done.add(j1);
+     done.add(j2);
+     done.add(j3);
+     done.add(j4);
+
+     for (HashSett  pro : done ){
+         System.out.println(pro.name);
+     }
+
+
+    }}
