b9ec78dfbc95a90614c8ef79af8e1e700b70be40
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-22 14:26:16 +0530

    practice

---
+++ src/CollectionFrameworks/Tree1.java
@@ -0,0 +1,29 @@
+package CollectionFrameworks;
+
+import java.util.TreeSet;
+
+public class Tree1 {
+int name ;
+    public Tree1(int  name ){
+        this.name = name ;
+
+    }
+}
+class Tree2 {
+    public static void main (String [] main ) {
+
+        TreeSet<Tree1> run = new TreeSet<>();
+        Tree1 t1 = new Tree1(90);
+        Tree1 t2 = new Tree1(11);
+        Tree1 t3 = new Tree1(33);
+
+        run.add(t1);
+        run.add(t2);
+        run.add(t3);
+        //     run.add(t2);
+
+            System.out.println(run);
+
+// not working it need to comprable for running
+    }
+}
---
+++ src/Lambda/Modern.java
@@ -0,0 +1,18 @@
+package Lambda;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+public class Modern {
+    public static void main (String [] args){
+        ArrayList<Integer> marklist  = new ArrayList<>();
+        marklist.add(45);
+        marklist.add(22);
+         marklist.add(86);
+        marklist.add(55);
+
+System.out.println(marklist);
+
+
+    }
+
+}
