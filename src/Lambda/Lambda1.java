6c9360d325586b5cf02651019ffd469702c42db0
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-22 16:54:54 +0530

    Lambda

--- src/Lambda/Lambda1.java
+++ src/Lambda/Lambda1.java
@@ -8,8 +8,12 @@ interface Mdo {
 public class Lambda1 {
     public  static void main (String [] args){
 
-        Mdo.addition = (p,q) -> (p+q);
+        Mdo  addition = (p,q) -> (p+q);
 
+        int b = 100;
+        int c = 200;
+        int r  = addition.calcy(b,c);
+        System.out.println(r);
 
 
     }
