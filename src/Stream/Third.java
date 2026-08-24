492d7dc1d8899246eafedbfad053093c8de1f52a
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-24 21:55:10 +0530

    understnding streams

--- src/Stream/Third.java
+++ src/Stream/Third.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Third {
     public static void main(String[] args){
 
-        List<Integer> list = Arrays.asList(3,44,55,222,55,88,2,99,400,2,333);
+        List<Integer> list = Arrays.asList(1,2,3,44,55,222,55,88,2,99,400,2,333);
 
         List<Integer> mystream = list.stream().
                 filter(x -> x % 2 == 0).
