64e32003f6cba274c28c3f07542cc655a04936ba
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-24 22:10:44 +0530

    instance block

--- src/Anotations/Frist.java
+++ src/Anotations/Frist.java
@@ -1,4 +1,15 @@
 package Anotations;
 
 public class Frist {
+
+
+    {
+    System.out.println("This is the end..!");
+}
+
+    public static void main(String [ ] args) {
+
+        Frist f = new Frist();
+
+    }
     }
