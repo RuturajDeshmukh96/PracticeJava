a801cd66ea8611b247a4ddededa399d65ed5a972
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-25 20:54:59 +0530

    run array

---
+++ src/Anotations/TypeShit.java
@@ -0,0 +1,17 @@
+//package Anotations;
+//@
+//@Deprecated
+//@FunctionalInterface
+//public interface TypeShit {
+//    void m1();//in
+//}
+////annotaion = sticker == means attached to the class ,variable , methods why is it used = to tell the jvm exta information
+////main concepts in the annotation
+////Retention type = where the annotaion will legit
+////                  1.Compilr time
+////                  2.Runntimme
+////                  3.Source loading
+//
+////Target Type = why used to tell the where it should it be used
+//
+//               //     Classes,mathiods ,variables,fields ,
---
+++ src/CollectionFrameworks/Ak_47.java
@@ -0,0 +1,26 @@
+package CollectionFrameworks;
+import java.util.ArrayList;
+public class Ak_47 {
+    String name;
+    public Ak_47 (String name ){
+        this.name = name;
+        System.out.println("Arrays");
+    }
+
+}
+class Run {
+    public static void main(String[] args) {
+
+        ArrayList<Ak_47> go = new ArrayList<>();
+        Ak_47 a = new Ak_47("rutu");
+        Ak_47 a2 = new Ak_47("raj");
+
+        go.add(a);
+        go.add(a2);
+
+        for (Ak_47 d  : go){
+            System.out.println(d.name);
+        }
+
+    }
+}
