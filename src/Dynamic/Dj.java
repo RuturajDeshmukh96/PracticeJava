a144ff6e6057f7657f6f0ca646e1140d43221422
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-26 23:20:08 +0530

    push

---
+++ src/Dynamic/Dj.java
@@ -0,0 +1,34 @@
+package Dynamic;
+
+ class Dynamic1 {
+
+    public void one() {
+        System.out.println("One");
+    }
+
+    public void two() {
+        System.out.println("two");
+    }
+
+  static   class Dynamo extends Dynamic1 {
+        public void three() {
+            System.out.println("three");
+        }
+
+        public void four() {
+            System.out.println("four");
+        }
+    }
+}
+public  class Dj {
+    public static void main(String[] args) {
+
+        Dynamic1.Dynamo d = new Dynamic1.Dynamo();
+
+        d.one();    // ✅
+        d.two();    // ✅
+        d.three();  // ✅
+        d.four();   // ✅
+
+    }
+}
