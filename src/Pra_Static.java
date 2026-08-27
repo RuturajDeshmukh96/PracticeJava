c7cd432652a8f8febb10428c6b8bc33f3057c715
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-27 23:27:57 +0530

    go

--- src/Pra_Static.java
+++ src/Pra_Static.java
@@ -9,16 +9,23 @@ public class Pra_Static {
     }
 }
 class Done {
-    static String name ;
-    static int age ;
+     String name ;
+     int age ;
+     static  int count = 0 ;
     public Done (String name , int age ){
         this.name=name ;
         this . age = age ;
-        System.out.println(name + "  "+age  );
+        count++;
+        System.out.println(name );
+             //   ""+age  );
+        System.out.println(age );
+        System.out.println(count);
     }
 
     static void main(String [] srgd ) {
         Pra_Static p = new Pra_Static();
         Done d = new Done("Ruturaj", 14);
+        Done d2 = new Done("rut",23);
+        System.out.println(Done.count);
     }
 }
