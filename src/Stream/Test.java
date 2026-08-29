15b412f3fda275230573839dfd0da845d04af13c
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-29 22:03:07 +0530

    go

--- src/Stream/Test.java
+++ src/Stream/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 
         List<Integer> done = Arrays.asList(11, 22, 11, 121, 1, 22, 55, 84, 5, 5, 516, 156, 456, 4866, 55, 15, 511, 11, 54);
-        List<Integer> my = done.stream().filter(x -> x % 2 == 0).map(x -> x * 2).distinct().sorted().toList();
+        List<Integer> my = done.stream().filter(x -> x % 2 == 0).map(x -> x + 2).distinct().sorted().toList();
 
         System.out.println(my);
     }
