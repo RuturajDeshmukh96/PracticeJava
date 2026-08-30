c91b090ad8205796f70a0e766faade2d28b94620
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-30 23:09:58 +0530

    go

--- src/Chaining/Chain2.java
+++ src/Chaining/Chain2.java
@@ -12,8 +12,8 @@ package Chaining;
          this.name = name;
          this.age = age;
          this.score = score;
-         System.out.println("Lets start Constuctor Chaining ");
-         System.out.println("The Constructor Chaining is in Working \n" + "It takes name age & score from the default constructor  : " + name + " " + age + " "  + score );
+         System.out.println(" Lets start Constuctor Chaining ");
+         System.out.println(" The Constructor Chaining is in Working \n" + "It takes name age & score from the default constructor  : " + name + " " + age + " "  + score );
      }
      public static void main(String[] srgs) {
          Chain2 w = new Chain2();
