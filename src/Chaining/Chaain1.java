8b12bf634f9a18bed9a802c0d5617336d3f5adc9
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-30 23:08:19 +0530

    go

--- src/Chaining/Chaain1.java
+++ src/Chaining/Chaain1.java
@@ -10,21 +10,21 @@ public class Chaain1 {
         return this ;
     }
     public Chaain1 m2 (int age ) {
-        System.out.println("The second methood - age : " + age );
+        System.out.println(" The second methood - age : " + age );
         return this ;
     }
     public Chaain1 m3  (float marks ){
-        System.out.println("The third methood - marks : " + marks );
+        System.out.println(" The third methood - marks : " + marks );
         return this ;
     }
     public  Chaain1 m4 (String pass) {
-        System.out.println("The fourth methood - pass or fails : " + pass);
+        System.out.println(" The fourth methood - pass or fails : " + pass);
         return this ;
     }
     public static void main (String [] args ){
         Chaain1 c = new Chaain1 ();
         c.m1("Ruturaj").m2(22).m3(89.00f).m4("pass");
-        System.out.println("The Method Chaining is running smoothly ");
+        System.out.println(" The Method Chaining is running smoothly ");
     }
 }
 
