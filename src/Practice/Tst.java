411144850e304a1202fc8b50e05a41184a00f81e
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-31 23:21:37 +0530

    practice dsa

---
+++ src/Practice/Tst.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Tst {
+    static void main() {
+        int num1 = 10 ;
+        int num2 = 20 ;
+        if (num1 + num2 >= 50){
+            System.out.println("hurrayy");
+        }else {
+            System.out.println("nope ");
+        }
+    }
+}
