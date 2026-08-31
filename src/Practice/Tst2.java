adf4f22c45e65d4889ebd7c25265850670c49eda
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-08-31 23:28:26 +0530

    practice dsa

---
+++ src/Practice/Tst2.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class Tst2 {
+    static void main() {
+        int a = 11 ;
+        int b = 2 ;
+        int c = 3 ;
+        //int d = 4 ;
+
+        if (a > b ){
+            System.out.println("a is great ");
+        }else if (b > c ){
+            System.out.println("b is great ");
+        }else if (c > a ){
+            System.out.println("c is great ");
+        }else   {
+            System.out.println(" enter valid no ...");
+        }
+    }
+}
