67428d47b7f0839cab33b1fae274106c4938aa86
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-01 23:23:46 +0530

    psuh

---
+++ src/DSA/St.java
@@ -0,0 +1,16 @@
+package DSA;
+
+public class St {
+  public   static void main(String[] args) {
+        int a = 100;
+
+
+        for  (int i = 1 ; i    < a; i ++ ){
+         for   (int j = 1 ; j <= i ; j ++){
+                System.out.print("*");
+            }
+        }
+        }
+
+    }
+
