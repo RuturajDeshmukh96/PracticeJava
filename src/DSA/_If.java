d5e38661064dd4159736958fe3d12704c2fb3037
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-01 22:20:34 +0530

    psuh

---
+++ src/DSA/_If.java
@@ -0,0 +1,18 @@
+package DSA;
+import java.util.Scanner;
+public class _If {
+
+    static void main() {
+        Scanner  sc = new Scanner(System.in);
+        int a = 110 ;
+        int b = 20 ;
+        int c = 30 ;
+        if (a > b ){
+            System.out.println("a is great ");
+        }else if (b > c ){
+            System.out.println("b is great ");
+        }else if (c > a ){
+            System.out.println("c is great ");
+        }
+    }
+}
