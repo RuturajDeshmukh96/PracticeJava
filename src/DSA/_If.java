e3ece1b7d4029acaef17e1ec7a7c341d46a49692
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-01 22:23:12 +0530

    psuh

--- src/DSA/_If.java
+++ src/DSA/_If.java
@@ -4,9 +4,13 @@ public class _If {
 
     static void main() {
         Scanner  sc = new Scanner(System.in);
-        int a = 110 ;
-        int b = 20 ;
-        int c = 30 ;
+        System.out.println("1st number a");
+        int a = sc.nextInt();
+        System.out.println("2nd number b");
+        int b = sc.nextInt();
+        System.out.println("3rd number c");
+        int c = sc.nextInt();
+        ;
         if (a > b ){
             System.out.println("a is great ");
         }else if (b > c ){
