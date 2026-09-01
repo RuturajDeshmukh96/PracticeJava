d4c86e3d1ca4530c607cae1bd942f49b17041c80
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-01 22:59:10 +0530

    psuh

---
+++ src/DSA/Whi.java
@@ -0,0 +1,14 @@
+package DSA;
+
+public class Whi {
+    static void main() {
+
+        int a = 15 ;
+        while (a >10){
+            System.out.println("a greater  than 10");
+            //System.out.println(a);
+            a--;
+            break;
+        }
+        }
+    }
