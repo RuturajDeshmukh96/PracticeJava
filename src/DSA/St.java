a47014054fdadfef3cfa6710f8865045071af1a1
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-02 13:24:18 +0530

    star  pyramid

--- src/DSA/St.java
+++ src/DSA/St.java
@@ -5,10 +5,11 @@ public class St {
         int a = 100;
 
 
-        for  (int i = 1 ; i    < a; i ++ ){
-         for   (int j = 1 ; j <= i ; j ++){
+        for  (int i = 0 ; i <= a; i ++ ){
+         for   (int j = 0 ; j <= i ; j ++){
                 System.out.print("*");
             }
+            System.out.println("  ");
         }
         }
 
