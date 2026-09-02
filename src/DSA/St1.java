99d8ea3ed120fa55496b7e46b0fd56d9e07debe7
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-02 13:20:47 +0530

    reverse pyramid

--- src/DSA/St1.java
+++ src/DSA/St1.java
@@ -4,8 +4,10 @@ public class St1 {
 
     public static void main (String [] args ){
         int n = 5 ;
-        for (int i = 0 ; i < n ; i++){
-            for (int j = 0 ; j< i ; j++){
+        // out loop
+        for (int i = n ; i >= 0; i--){
+            // inner loop
+            for (int j = 0 ; j<=i ; j++){
                 System.out.print("*");
             }
             System.out.println("  ");
