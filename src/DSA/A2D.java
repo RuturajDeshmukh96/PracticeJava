9857e76bb6ccf743fd2f6754e5137f2720c9619d
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-02 22:15:24 +0530

    array2d

---
+++ src/DSA/A2D.java
@@ -0,0 +1,26 @@
+package DSA;
+import java.util.*;
+public class A2D {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the row number : ");
+        int rows = sc.nextInt();
+        System.out.println("enter the column number :");
+        int col = sc.nextInt();
+        System.out.println("enter elements in your defined matrix ");
+        int Ar2 [] [] = new int[rows][col];
+
+       for (int i = 0 ; i< rows ; i ++) {
+           for (int j = 0; j < col; j++) {
+               Ar2[i][j] = sc.nextInt();
+           }
+       }
+           for (int i = 0 ; i < rows; i ++) {
+               for (int j = 0 ; j < col; j++ ){
+                   System.out.print(Ar2 [i] [j] + " ");
+               }
+               System.out.println();
+           }
+       }
+
+}
