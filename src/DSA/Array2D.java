11c25e8e3718fd8f5c917aa5d41fa6fade3483b2
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-02 17:21:32 +0530

    array2d

--- src/DSA/Array2D.java
+++ src/DSA/Array2D.java
@@ -1,25 +1,29 @@
-package DSA;
-import java.util.*;
-public class Array2D {
-    static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-  int rows = sc.nextInt();
-  int colms = sc.nextInt();
+    package DSA;
+    import java.util.*;
+    public class Array2D {
+        static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+            System.out.println("enter rows number");
+      int rows = sc.nextInt();
+            System.out.println("enter column number");
+      int colms = sc.nextInt();
 
-  int TwoD [] [] = new int [rows] [colms];
-     //row
-  for (int i = 0 ; i <rows;i++){
-      //column
-      for (int j = 0 ; j < colms;j++){
-   TwoD [i] [j] = sc.nextInt();
+      int TwoD [] [] = new int [rows] [colms];
+         //row
+            System.out.println("enter rows ele " + (rows* colms) + " enter colmn ele");
+      for (int i = 0 ; i <rows;i++){
+          //column
+          for (int j = 0 ; j < colms;j++){
+       TwoD [i] [j] = sc.nextInt();
+          }
+
       }
-
-  }
-  //output
-        for (int i = 0 ; i <rows;i++) {
-            for (int j = 0 ; j<colms;j++){
-                System.out.println(TwoD[i][j]+ "");
+      //output
+            for (int i = 0 ; i <rows;i++) {
+                for (int j = 0 ; j<colms;j++){
+                    System.out.print(TwoD[i][j]+ " ");
+                }
+                System.out.println();
             }
         }
     }
-}
