f1a703fbacd6f3f14909b1b628453f3f7abd54b6
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-02 15:03:03 +0530

    star  pyramid

---
+++ src/DSA/Array.java
@@ -0,0 +1,19 @@
+package DSA;
+import java.util.Arrays;
+public class Array {
+    public static void main(String [] args ) {
+        int [] marks = new int  [5];
+
+        marks [0] = 23;
+         marks [1] = 23;
+         marks [2] = 23;
+         marks [3] = 23;
+         marks [4] = 23;
+
+        for (int i = 0 ; i < 5 ; i ++){
+            System.out.println(marks[i]);
+        }
+
+
+    }
+}
