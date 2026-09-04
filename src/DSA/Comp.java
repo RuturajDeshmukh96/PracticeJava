cf4042678c6fbbf712bbd2e8a81356fba273aaaa
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-04 07:32:03 +0530

    array

---
+++ src/DSA/Comp.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class Comp {
+    public static void main (String[] args){
+        int a = 56;
+        int b = 89;
+        if(a == b){
+            System.out.println("same");
+        }else{
+            System.out.println("not same");
+        }
+    }
+}
--- src/DSA/Compare_String.java
+++ src/DSA/Compare_String.java
@@ -2,8 +2,8 @@ package DSA;
 
 public class Compare_String {
     public static void main (String [] args){
-        String  name1 = "Rutu";
-        String name2 = "Ruturaj";
+        String  name1 = "5";
+        String name2 = "55";
         if (name1.compareTo(name2)==0){
             System.out.println("Both Strings are same ");
         }else {
