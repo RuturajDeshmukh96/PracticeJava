a07ef47e4b2e637d96fe389211e5b90cde46fd0a
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-07 22:37:53 +0530

    array

--- src/Practice/Cal.java
+++ src/Practice/Cal.java
@@ -2,8 +2,8 @@ package Practice;
 
 public class Cal {
     static void main() {
-
+           double f = 90.5f;
         int n = 5;
-        System.out.println(n);
+        System.out.println(n + f );
     }
 }
