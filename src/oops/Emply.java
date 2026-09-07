44947551ae00522b4854159a2054c50d1cc9aaec
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-07 22:04:48 +0530

    array

--- src/oops/Emply.java
+++ src/oops/Emply.java
@@ -16,7 +16,7 @@ public abstract  class Emply {
           return  this.Name = Name  ;
     }
     public void setId (int Id){
-        this.Id = Id ;
+       this.Id = Id ;
     }
     public int getId1 () {
         return this.Id = Id;
@@ -25,29 +25,31 @@ public abstract  class Emply {
 }
 
 
-class FullTimeEmployee extend Emply {
+class FullTimeEmployee extends Emply {
  private double weeklySalary ;
- public FullTimeEmployee (String name , int Id, double weeklySalary){
-     super(name ,Id);
-     this.weeklySalary=weeklySalary;
-
+ public FullTimeEmployee (String name , int Id, double weeklySalary) {
+     super(name, Id);
+     this.weeklySalary = weeklySalary;
+ }
      @Override
-      public void calculatePay(){
+      public double calculatePay(){
+         return this.weeklySalary ;
          System.out.println("Done Poly...1");
      }
- }
+
 }
 
-class PartTimeEmployee extend  Emply {
+class PartTimeEmployee extends  Emply {
     private double hoursrate ;
     private int hourswork;
-public PartTimeEmployee (String name , int Id ,double hoursrate , int hourswork){
-    super(name , Id);
-    this.hoursrate = hoursrate ;
-    this.hourswork = hourswork ;
-   @Override
-      public void calculatePay(){
-        System.out.println("Done Poly...");
-    }
+public PartTimeEmployee (String name , int Id ,double hoursrate , int hourswork) {
+    super(name, Id);
+    this.hoursrate = hoursrate;
+    this.hourswork = hourswork;
 }
+   @Override
+      public int  calculatePay(){
+        System.out.println("Done Poly...");
+       return this.hourswork * this.hourswork;
+    }
 }
