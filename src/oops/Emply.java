48b650fa2dc1b366cb12eb7cd5bfe66c543b9b6f
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-07 20:52:33 +0530

    array

--- src/oops/Emply.java
+++ src/oops/Emply.java
@@ -4,22 +4,50 @@ public abstract  class Emply {
     private String Name ;
     private int Id;
 
+
     public Emply (String Name , int Id){
         this.Name = Name;
         this.Id = Id;
     }
-
-    public void setName(String name) {
+    public void setName(String Name) {
         this.Name = Name;
     }
-
-    public void getName () {
-          this.Name ;
+    public String  getName1 () {
+          return  this.Name = Name  ;
     }
     public void setId (int Id){
         this.Id = Id ;
     }
-    public void getId () {
-        this.Id = Id;
+    public int getId1 () {
+        return this.Id = Id;
     }
+    public abstract void calculatePay () ;
+}
+
+
+class FullTimeEmployee extend Emply {
+ private double weeklySalary ;
+ public FullTimeEmployee (String name , int Id, double weeklySalary){
+     super(name ,Id);
+     this.weeklySalary=weeklySalary;
+
+     @Override
+      public void calculatePay(){
+         System.out.println("Done Poly...1");
+     }
+ }
+}
+
+class PartTimeEmployee extend  Emply {
+    private double hoursrate ;
+    private int hourswork;
+public PartTimeEmployee (String name , int Id ,double hoursrate , int hourswork){
+    super(name , Id);
+    this.hoursrate = hoursrate ;
+    this.hourswork = hourswork ;
+   @Override
+      public void calculatePay(){
+        System.out.println("Done Poly...");
+    }
+}
 }
