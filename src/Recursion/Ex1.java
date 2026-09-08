b80128458cd57a22af1b8e0ad484a7f19f6e9ac9
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-08 18:05:00 +0530

    psuh

---
+++ src/Recursion/Ex1.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.sql.SQLOutput;
+
+public class Ex1 {
+    public static  void Fast (int n) {
+        if (n == 0 ) {
+            System.out.println("Go..!");
+            return;
+        }
+        System.out.println(n);
+
+        Fast(n-1 );
+    }
+    public static void main (String [] args ) {
+        System.out.println("lets start countdown ");
+        Fast(10);
+    }
+}
