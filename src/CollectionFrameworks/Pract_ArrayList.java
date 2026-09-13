cc3190a83aee6efc6efc45134a34eca1277c9d61
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-13 23:46:24 +0530

    pract

---
+++ src/CollectionFrameworks/Pract_ArrayList.java
@@ -0,0 +1,17 @@
+package CollectionFrameworks;
+import NEXT_CHAPTER.Array;
+
+import java.util.ArrayList;
+public class Pract_ArrayList {
+    public void start() {
+        System.out.println("lets start to print the arrayList");
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<String> done = new ArrayList<>();
+
+    done.add("go");
+    done.add("One");
+}
+}
