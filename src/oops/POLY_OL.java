e043cb0eacc1313396fddc63fa7a24cf8070c3c4
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-17 23:23:44 +0530

    push

--- src/oops/POLY_OL.java
+++ src/oops/POLY_OL.java
@@ -1,9 +1,12 @@
 package oops;
 
+import java.sql.SQLOutput;
+
 public class POLY_OL {
     String name;
     int age;
     float marks;
+    String Collagename ;
 
     public void Print(String name) {
         System.out.println(name);
@@ -17,18 +20,32 @@ public class POLY_OL {
     public void Print(Float marks) {
         System.out.println(marks);
     }
+    public void Danger (String Collagename ){
+        System.out.println(Collagename);
+    }
 }
- class Do {
+ class Do extends POLY_OL {
+     @Override
+     public void  Danger (String Collagename ){
+         System.out.println("MIT");
+         // return this ;
+     }
     public static void main(String[]args ) {
-        POLY_OL p1 = new POLY_OL();
+
+        POLY_OL p1 = new Do();
+      //  POLY_OL p1 = new ();
         p1 . age = 21 ;
         p1 . name ="Ruturaj";
         p1. marks = 89.0f;
+        p1.Collagename = "Sveri";
         p1.Print(p1.name );
         p1.Print(p1.marks);
         p1.Print(p1.age);
+        p1.Danger(p1.Collagename);
+        p1.Danger(p1.Collagename);
 
     }
+
 }
 // Polymorphism
 // overloading
