1fd6aa4b2efe9c4667f527f353f7bd75908a0c69
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-22 23:19:49 +0530

    push

---
+++ src/Conditions/GO1.java
@@ -0,0 +1,16 @@
+package Conditions;
+
+public class GO1 {
+
+    public static void main (String[] args) {
+
+           int a = 10 ;
+         int b = 19 ;
+         int sum = a+b ;
+        do {
+            System.out.println(sum);
+
+        }
+        while (sum < 10  );
+    }
+}
