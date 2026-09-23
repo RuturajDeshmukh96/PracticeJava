dfa87073a52ea91c2c6f48e5d3ee8dee85e7e281
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-23 10:23:38 +0530

    push

--- src/Practice/Tet.java
+++ src/Practice/Tet.java
@@ -3,9 +3,7 @@ package Practice;
 public class Tet {
     public static void main (String [] args ){
 
-
-        int String = 10 ;
-        System.out.println(String );
+        int Integer = 10 ;
+        System.out.println(Integer);
     }
-
 }
