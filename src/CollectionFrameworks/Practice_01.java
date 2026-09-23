cb9736c4ea4f01ef63bedc67414b6fe50c8c1f79
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-23 21:07:45 +0530

    push

---
+++ src/CollectionFrameworks/Practice_01.java
@@ -0,0 +1,20 @@
+package CollectionFrameworks;
+import NEXT_CHAPTER.Array;
+
+import java.util.*;
+public class Practice_01 {
+    //int String ;
+
+
+    public static void main (String [] aegs ){
+        ArrayList <String> s1 = new ArrayList<>();
+    s1.add("RUn1") ;
+    s1.add("Attack");
+    s1.add("Run");
+
+    for (String p : s1) {
+        System.out.println(s1);
+    }
+
+}
+}
--- src/Practice/Tet.java
+++ src/Practice/Tet.java
@@ -1,8 +1,6 @@
 package Practice;
-
 public class Tet {
     public static void main (String [] args ){
-
         int Integer = 10 ;
         System.out.println(Integer);
     }
