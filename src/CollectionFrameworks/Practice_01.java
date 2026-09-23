0bdd39c4471a1e417b89411a5e8181fab8e1b9ad
ruturajdeshmukh0096 <ruturajdeshmukh0096@gmail.com>
2026-09-23 21:15:39 +0530

    push

--- src/CollectionFrameworks/Practice_01.java
+++ src/CollectionFrameworks/Practice_01.java
@@ -3,18 +3,30 @@ import NEXT_CHAPTER.Array;
 
 import java.util.*;
 public class Practice_01 {
-    //int String ;
+    String name;
 
-
-    public static void main (String [] aegs ){
-        ArrayList <String> s1 = new ArrayList<>();
-    s1.add("RUn1") ;
-    s1.add("Attack");
-    s1.add("Run");
-
-    for (String p : s1) {
-        System.out.println(s1);
+    public void OP(String name) {
+        System.out.println(name);
     }
+}
 
-}
-}
+    class done_2 {
+        public static void main(String[] aegs) {
+            ArrayList<String> s1 = new ArrayList<>();
+            // s1.add("RUn1") ;
+            //s1.add("Attack");
+            //s1.add("Run");
+            Practice_01 p1 = new Practice_01();
+            //System.out.println(p1.OP("RUtu");
+         p1.OP("Rutu");
+         p1.OP("RAJ");
+
+         s1.add(p1.name);
+
+
+            for (String p : s1) {
+                System.out.println(s1);
+            }
+
+        }
+    }
